af3174c3483f4268d23c9b075c2dcbeb94dd4a4b
Enigmatron <10167806+Enigmatron@users.noreply.github.com>
2018-10-07 12:12:01 -0500

    Bluetooth Connection

--- Designstuff/app/src/main/java/com/example/youwin1234/designstuff/QRCodeGenFragment.java
+++ Designstuff/app/src/main/java/com/example/youwin1234/designstuff/QRCodeGenFragment.java
@@ -1,8 +1,15 @@
 package com.example.youwin1234.designstuff;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,13 +24,100 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.util.Set;
+
 public class QRCodeGenFragment extends android.support.v4.app.Fragment {
+
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    String code;
+
+
+
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)){
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BA.ERROR);
+                switch(state){
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d("Bluetooth", "STATE_ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d("Bluetooth", "STATE_TURNING_ON");
+
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d("Bluetooth", "STATE_OFF");
+
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d("Bluetooth", "STATE_TURNING_OFF");
+
+                        break;
+                }
+            }
+//            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                // Discovery has found a device. Get the BluetoothDevice
+//                // object and its info from the Intent.
+////                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+////                String deviceName = device.getName();
+////                String deviceHardwareAddress = device.getAddress(); // MAC address
+//            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        getActivity().unregisterReceiver(mReceiver);
+    }
+
+    public void on(View v) {
+//        Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        if (BA == null) {
+            Log.d("Bluetooth", " AdapterNull");
+
+            Toast.makeText(getContext(), "BA Null", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (!BA.isEnabled()) {
+                Log.d("Bluetooth", "Adapter is not Enabled");
+
+                Toast.makeText(getContext(), "BA Enabled", Toast.LENGTH_LONG).show();
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivity(enableIntent);
+                IntentFilter BIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+                getActivity().registerReceiver(mReceiver, BIntent);
+
+                //TODO: Is enabled continue with functionality for enabled devices...
+            }
+            if (BA.isEnabled()) {
+                Log.d("Bluetooth", "Adapter is Enabled");
+
+                Toast.makeText(getContext(), "BA Disable", Toast.LENGTH_LONG).show();
+
+                BA.disable();
+                IntentFilter BIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+                getActivity().registerReceiver(mReceiver, BIntent);
+            }
+        }
+    }
+
+
 
     private EditText etInput;
     private Button btnCreateQr;
@@ -38,17 +132,24 @@ public class QRCodeGenFragment extends android.support.v4.app.Fragment {
         etInput = (EditText) v.findViewById(R.id.etInput);
         btnCreateQr = (Button) v.findViewById(R.id.btnCreate);
         imageView = (ImageView) v.findViewById(R.id.imageView);
+        BA = BluetoothAdapter.getDefaultAdapter();
+        code = BA.getAddress();
+//            Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+//            startActivityForResult(getVisible,0);
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        getActivity().registerReceiver(mReceiver, filter);
+
 
         btnCreateQr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = etInput.getText().toString().trim();
 
-                if(text != null){
+                if(code != null){
                     MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
 
                     try {
-                        BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,
+                        BitMatrix bitMatrix = multiFormatWriter.encode(code, BarcodeFormat.QR_CODE,
                                 1000, 1000);
                         BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
                         Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
@@ -59,6 +160,9 @@ public class QRCodeGenFragment extends android.support.v4.app.Fragment {
                 }
             }
         });
+
+
+
         return v;
 
     }
--- Designstuff/app/src/main/java/com/example/youwin1234/designstuff/QRScan.java
+++ Designstuff/app/src/main/java/com/example/youwin1234/designstuff/QRScan.java
@@ -1,11 +1,15 @@
 package com.example.youwin1234.designstuff;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +19,19 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
 public class QRScan extends android.support.v4.app.Fragment {
-
+    UUID myuuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    private static final String TAG = "QRScan";
     private Button scan_btn;
+    BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
 
-
+    BluetoothDevice device = null;
+    BluetoothSocket tmp = null;
+    BluetoothSocket mmSocket = null;
 
 
     @Override
@@ -41,6 +53,8 @@ public class QRScan extends android.support.v4.app.Fragment {
             }
         });
         Intent intent = new Intent(getActivity(), QRScan.class);
+
+
         //getActivity().startActivityForResult(intent, 12345);
         return v;
     }
@@ -86,6 +100,23 @@ public class QRScan extends android.support.v4.app.Fragment {
             }
             else {
                 Toast.makeText(getContext(), result.getContents(), Toast.LENGTH_LONG).show();
+                try {
+                    device = BA.getRemoteDevice(result.getContents());
+                    tmp = device.createRfcommSocketToServiceRecord(myuuid);
+                    try {
+                        Method m = device.getClass().getMethod("createRfcommSocket", new Class[]{int.class});
+                        tmp = (BluetoothSocket) m.invoke(device, 1);
+                        Log.e(TAG, "CONNECTION SUCCESSFUL");
+
+                    }
+                    catch(Exception e) {
+                        Log.e(TAG, "Creation Error");
+
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "create() failed", e);
+                }
+                mmSocket = tmp;
             }
         }
         else {

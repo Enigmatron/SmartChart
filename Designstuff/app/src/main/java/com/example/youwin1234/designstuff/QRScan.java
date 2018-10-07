262cae5853eefb3ba5674e0874b8221bf616f5f2
Enigmatron <10167806+Enigmatron@users.noreply.github.com>
2018-10-06 22:20:02 -0500

    Scan commit again
    cuz we missed stuff the first time

--- Designstuff/app/src/main/java/com/example/youwin1234/designstuff/QRScan.java
+++ Designstuff/app/src/main/java/com/example/youwin1234/designstuff/QRScan.java
@@ -2,29 +2,35 @@ package com.example.youwin1234.designstuff;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+public class QRScan extends android.support.v4.app.Fragment {
 
     private Button scan_btn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qrscan);
 
-        scan_btn = (Button) findViewById(R.id.scan_btn);
-        final Activity activity = this;
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_qrscan, container, false);
+        scan_btn = (Button) v.findViewById(R.id.scan_btn);
+//        final Activity activity = v;
         scan_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
+                FragmentIntentIntegrator integrator = new FragmentIntentIntegrator(getSelf());
                 integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
                 integrator.setPrompt("Scan");
                 integrator.setCameraId(0);
@@ -34,18 +40,56 @@ import com.google.zxing.integration.android.IntentResult;
 
             }
         });
+        Intent intent = new Intent(getActivity(), QRScan.class);
+        //getActivity().startActivityForResult(intent, 12345);
+        return v;
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    android.support.v4.app.Fragment getSelf(){
+        return this;
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    /*
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_qrscan);
+
+            scan_btn = (Button) findViewById(R.id.scan_btn);
+            final Activity activity = this;
+            scan_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    IntentIntegrator integrator = new IntentIntegrator(activity);
+                    integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                    integrator.setPrompt("Scan");
+                    integrator.setCameraId(0);
+                    integrator.setBeepEnabled(false);
+                    integrator.setBarcodeImageEnabled(false);
+                    integrator.initiateScan();
+
+                }
+            });
+        }
+        */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null){
             if (result.getContents() == null){
-                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "You cancelled the scanning", Toast.LENGTH_LONG).show();
+
             }
             else {
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), result.getContents(), Toast.LENGTH_LONG).show();
             }
         }
         else {
+
             super.onActivityResult(requestCode, requestCode, data);
         }
     }

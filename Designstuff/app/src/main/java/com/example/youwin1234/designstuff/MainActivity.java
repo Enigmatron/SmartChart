349f366562a4dbe7754b82a06ef9544bfdbedb4c
Enigmatron <10167806+Enigmatron@users.noreply.github.com>
2018-10-06 22:43:08 -0500

    Image stuff and minor bug fix on nav bar

--- Designstuff/app/src/main/java/com/example/youwin1234/designstuff/MainActivity.java
+++ Designstuff/app/src/main/java/com/example/youwin1234/designstuff/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if(savedInstanceState == null){
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
-        navigationView.setCheckedItem(R.id.nav_QRCodeGen);}
+        navigationView.setCheckedItem(R.id.nav_profile);}
     }
 
     @Override
